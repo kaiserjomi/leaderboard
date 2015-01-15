5f8e56f4fd72b5c3167505d199d8f5e54940db8d
Kaiser <jomi.man@gmail.com>
2015-01-15 10:27:59 +0000

    pagina 57 antes de Events

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,26 @@
 console.log("Hello World.");
+
+//criamos uma coleção chamada "players" na variavel global "Playerlist"
+PlayersList = new Meteor.Collection('players');
+
+if(Meteor.isClient){
+  //this code only runs on the client
+Template.leaderboard.helpers({
+  'player': function(){
+    return PlayersList.find()
+  },
+  'conta': function(){
+    return PlayersList.find().count();
+  },
+  'otherHelperFunction': function(){
+    return "some other function"
+}
+});
+  
+}
+
+
+if(Meteor.isServer){
+  //this code only runs on the server
+
+}
